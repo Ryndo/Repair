91c194dcc9ac1a4ada4f3de066d1931a4fba0452
Akytho <cyrilgajan@gmail.com>
2020-02-01 03:39:25 +0100

    QTE Stance, direction et combo

--- Assets/Scripts/SpaceShipControls.cs
+++ Assets/Scripts/SpaceShipControls.cs
@@ -7,17 +7,45 @@ public class SpaceShipControls : MonoBehaviour
 {
     QTEManager qte_Manager;
     PlayerInput playerInput;
+    enum Stance { Attack , Repair};
+    Stance stance;
+    PartsManager partsManager;
+    Player player;
+    int enemyId;
 
     void Awake(){
         qte_Manager = GetComponent<QTEManager>();
         playerInput = GetComponent<PlayerInput>();
+        partsManager = PartsManager.instance;
+        player = GetComponent<Player>();
+        enemyId = player.id == 0 ? 1 : 0;
     }
-    void OnShot(){
+    void StartQTE(){
         playerInput.SwitchCurrentActionMap("QTE");
         qte_Manager.GenerateQTE();
     }
 
-    public void QuitQTE(){
+    public void QuitQTE(PartsManager.PARTS targetedPart,int combo){
         playerInput.SwitchCurrentActionMap("Player");
+        ExecuteAction(targetedPart,combo);
+
+    }
+    void OnAttack(){
+        stance = Stance.Attack;
+        StartQTE();
+    }
+    void OnRepair(){
+        stance = Stance.Repair;
+        StartQTE();
+    }
+
+    void ExecuteAction(PartsManager.PARTS targetedPart,int combo){
+        int amount = player.actionAmount + combo;
+        if(stance == Stance.Attack){
+            partsManager.DamagePart(enemyId,targetedPart,amount);
+        }
+        else{
+            partsManager.RepairPart(player.id,targetedPart,amount);
+        }
     }
 }
