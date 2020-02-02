b2b3062a903bc8744b30aac33f14fd6aa0af53b0
AnthoCuttivet <anthony.cuttivet@gmail.com>
2020-02-02 10:54:52 +0100

    Music UI

---
+++ Assets/Scripts/Juice/MusicJuice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MusicJuice : MonoBehaviour
+{
+
+    public float offset;
+    public float duration;
+
+    // Start is called before the first frame update
+    void Start(){
+        Sequence s = DOTween.Sequence();
+        s.Append(gameObject.GetComponent<Image>().transform.DOLocalMoveX(gameObject.transform.position.x - offset, duration).SetEase(Ease.OutQuint))
+        .Append(gameObject.GetComponent<Image>().transform.DOLocalMoveX(gameObject.transform.localPosition.x, duration).SetEase(Ease.OutQuint));
+    }
+}
